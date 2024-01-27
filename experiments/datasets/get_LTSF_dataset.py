27df3f368389782c6f3abd8a6cb58530d4da9105
dengdifan <difandeng@gmail.com>
2024-01-27 17:42:34 +0100

    move exchange to small dataset

--- experiments/datasets/get_LTSF_dataset.py
+++ experiments/datasets/get_LTSF_dataset.py
@@ -8,8 +8,8 @@ import os
 from sklearn.preprocessing import StandardScaler
 import warnings
 
-SMALL_DATASET = ['ETTh1', 'ETTh2', 'illness', 'ETTm1', 'ETTm2', 'weather']
-LARGE_DATASET = ['traffic', 'exchangerate', 'electricity']
+SMALL_DATASET = ['ETTh1', 'ETTh2', 'illness', 'ETTm1', 'ETTm2', 'weather', 'exchangerate']
+LARGE_DATASET = ['traffic', 'electricity']
 
 
 DATASETS_INFO = {
